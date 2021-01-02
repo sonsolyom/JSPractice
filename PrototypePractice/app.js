6540c55ad754d753c58ffc83ea0c926d6adfca05
sonsolyom <nagydsolyom@gmail.com>
2021-01-02 09:39:39 +0100

    Update practice app.js with another exercise

--- PrototypePractice/app.js
+++ PrototypePractice/app.js
@@ -43,7 +43,7 @@ console.log(mary.getFullName()); */
 
 
 /* Protorype Inheritence */
-//Person constructor
+/* //Person constructor
 function Person(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -79,4 +79,33 @@ Customer. prototype.greeting = function(){
   return `Hello there, ${this.firstName} ${this.lastName}, welcome to our company!`;
 }
 
-console.log(customer1.greeting());
+console.log(customer1.greeting()); */
+
+/* Using Object.create */
+const personPrototypes = {
+  greeting: function() {
+    return `Hello there, ${this.firstName} ${this.lastName}!`;
+  },
+  getsMarried: function(newLastName){
+    this.lastName = newLastName;
+  }
+}
+
+const mary = Object.create(personPrototypes);
+mary.firstName = 'Mary';
+mary.lastName = 'Williams';
+mary.age = 30;
+
+// console.log(mary);
+
+mary.getsMarried('Thompson');
+console.log(mary.greeting());
+
+const brad = Object.create(personPrototypes, {
+  firstName: {value: 'Brad'},
+  lastName: {value: 'Traversy'},
+  age: {value: 36},
+});
+
+console.log(brad);
+console.log(brad.greeting());
